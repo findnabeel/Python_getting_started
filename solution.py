282e244459ad4cdfa9fb6747be039b528a823e36
findnabeel <findnabeel@gmail.com>
2020-12-07 23:56:11 +0400

    Add files via upload

--- solution.py
+++ solution.py
@@ -19,14 +19,15 @@ def welcome_assignment_answers(question):
   elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
     answer = "No"
   elif question == "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number":
-    answer = 4 + 1
+    answer = str(4 + 1)
   elif question == "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number":
-    answer = 3 + 1
+    answer = int(3 + 1)
   return(answer)
 # Complete all the questions.
 
 
 if __name__ == "__main__":
     #use this space to debug and verify that the program works
-    debug_question = "Are encoding and encryption the same? - Yes/No"
+    debug_question = "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number"
+    #debug_question = "Are encoding and encryption the same? - Yes/No"
     print(welcome_assignment_answers(debug_question))
